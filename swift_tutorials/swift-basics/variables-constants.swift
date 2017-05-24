a1a36268ca0e00370f4bcb4f30f7b9e763d66daa
Emily <e.a.moses@gmail.com>
2017-05-23 19:30:53 -0700

    variables and constants in swift

---
+++ swift_tutorials/swift-basics/variables-constants.swift
@@ -0,0 +1,16 @@
+var alwaysString = "Hello, world"
+
+alwaysString = "This is another string"
+
+var alwaysNumber = 10
+
+alwaysNumber = 20
+
+
+// Constants
+
+let language = "Swift"
+
+// Naming constants
+
+let programmingLanguage = "Objective-C"
